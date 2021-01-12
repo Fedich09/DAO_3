5d3f0b52b8a876ae6ac4196f3ca02169f1e234d0
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-01-12 20:56:06 +0200

    added implementation

---
+++ src/main/java/mate/service/DriverService.java
@@ -0,0 +1,16 @@
+package mate.service.impl;
+
+import mate.model.Driver;
+import java.util.List;
+
+public interface DriverService {
+    Driver create(Driver driver);
+
+    Driver get(Long id);
+
+    List<Driver> getAll();
+
+    Driver update(Driver driver);
+
+    boolean delete(Long id);
+}
