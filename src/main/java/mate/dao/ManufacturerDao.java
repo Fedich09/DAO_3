f2b3acba999ff03374d59d0489d288fb7e65ba4b
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-01-12 21:07:35 +0200

    fixed code style

--- src/main/java/mate/dao/ManufacturerDao.java
+++ src/main/java/mate/dao/ManufacturerDao.java
@@ -1,6 +1,19 @@
 package mate.dao;
 
+import java.util.List;
+import java.util.Optional;
 import mate.model.Manufacturer;
 
-public interface ManufacturerDao extends Dao<Manufacturer> {
+public interface ManufacturerDao {
+    Manufacturer add(Manufacturer manufacturer);
+
+    Optional<Manufacturer> getById(Long id);
+
+    Manufacturer update(Manufacturer object);
+
+    boolean deleteById(Long id);
+
+    boolean delete(Manufacturer manufacturer);
+
+    List<Manufacturer> getAll();
 }
--- src/main/java/mate/db/Storage.java
+++ src/main/java/mate/db/Storage.java
@@ -2,14 +2,30 @@ package mate.db;
 
 import java.util.ArrayList;
 import java.util.List;
+import mate.model.Car;
+import mate.model.Driver;
 import mate.model.Manufacturer;
 
 public class Storage {
     public static List<Manufacturer> manufacturers = new ArrayList<>();
+    public static List<Driver> drivers = new ArrayList<>();
+    public static List<Car> cars = new ArrayList<>();
     private static Long manufactureId = 0L;
+    private static Long driversId = 0L;
+    private static Long carsId = 0L;
 
     public static void addManufacturer(Manufacturer manufacturer) {
         manufacturer.setId(++manufactureId);
         manufacturers.add(manufacturer);
+    }
+
+    public static void addMDriver(Driver driver) {
+        driver.setId(++driversId);
+        drivers.add(driver);
+    }
+
+    public static void addCar(Car car) {
+        car.setId(++carsId);
+        cars.add(car);
     }
 }
